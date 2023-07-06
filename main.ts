5d47dd81b8240856a94475afa697dabbe4ab4f33
Daniel McGillick <56147357+DMacMakes@users.noreply.github.com>
2023-07-06 22:11:17 +1000

    sending forwards backwards to the robot on channel 1 when left stick is moved up or down.

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+let left_right = 0
+let forward_backward = 0
+radio.setGroup(1)
 basic.forever(function () {
     if (EMF_Gamepad.Button_status(EMF_Button.R_BUTTON, Button_State.JOYSTICK_PRESS_DOWN)) {
         basic.showLeds(`
@@ -54,9 +57,20 @@ basic.forever(function () {
             `)
     }
     if (EMF_Gamepad.Stick_position(Stick_Id.STICK_LEFT, Stick_Axis.STICK_Y) < 98) {
-        serial.writeLine("go down")
+        forward_backward = -1
+        radio.sendValue("forward_backward", forward_backward)
+        basic.pause(250)
     } else if (EMF_Gamepad.Stick_position(Stick_Id.STICK_LEFT, Stick_Axis.STICK_Y) > 148) {
-        serial.writeLine("go up")
+        forward_backward = 1
+        radio.sendValue("forward_backward", forward_backward)
+        basic.pause(250)
+    } else {
+    	
+    }
+    if (EMF_Gamepad.Stick_position(Stick_Id.STICK_LEFT, Stick_Axis.STICK_X) < 98) {
+        left_right = -1
+    } else if (EMF_Gamepad.Stick_position(Stick_Id.STICK_LEFT, Stick_Axis.STICK_X) > 148) {
+        left_right = 1
     } else {
     	
     }
